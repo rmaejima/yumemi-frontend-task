3a05e73309e6c40a6f9aaf851f6785bd09098f6e
rmaejima <ryo.maejii@gmail.com>
2022-11-08 21:25:29 +0900

    feat: init baseUrl

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "ress";
-import "./global.css";
-import App from "./App";
+import "global.css";
+import App from "App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
