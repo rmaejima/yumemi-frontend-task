eb6f1c2d65d5c6351a4859bc1817976314878031
rmaejima <ryo.maejii@gmail.com>
2022-11-09 17:31:52 +0900

    feat: init styled components theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,68 @@
+export const colors = {
+  text: {
+    dark: '#0f172a',
+    base: '#334155',
+    light: '#64748b',
+  },
+  primary: {
+    50: '#e1f2f1',
+    100: '#87ccc4',
+    200: '#87ccc4',
+    300: '#5bb8ac',
+    400: '#3ea89a',
+    500: '#2e9888',
+    600: '#2e9888',
+    700: '#267b6c',
+    800: '#226b5d',
+    900: '#194f42',
+  },
+  gray: {
+    50: '#f8fafc',
+    100: '#f1f5f9',
+    200: '#e2e8f0',
+    300: '#cbd5e1',
+    400: '#94a3b8',
+    500: '#64748b',
+    600: '#475569',
+    700: '#334155',
+    800: '#1e293b',
+    900: '#0f172a',
+  },
+  error: {
+    50: '#FEF2F2',
+    100: '#FEE2E2',
+    200: '#FECACA',
+    300: '#FCA5A5',
+    400: '#F87171',
+    500: '#EF4444',
+    600: '#DC2626',
+    700: '#B91C1C',
+    800: '#991B1B',
+    900: '#7F1D1D',
+  },
+};
+
+export const breaks = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+};
+
+export const breakNums = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+};
+
+export const easings = {
+  easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  easeOut: 'cubic-bezier(0.0, 0, 0.2, 1)',
+  easeIn: 'cubic-bezier(0.4, 0, 1, 1)',
+  sharp: 'cubic-bezier(0.4, 0, 0.6, 1)',
+};
+
+export const shadows = {
+  sm: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+  md: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+  lg: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+};
