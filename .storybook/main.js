5e7bcb7638bda0e26bbaa929ab5575f7d7be3db2
rmaejima <ryo.maejii@gmail.com>
2022-11-09 15:49:37 +0900

    feat: init storybook

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+module.exports = {
+  stories: [
+    {
+      directory: '../src/stories',
+      titlePrefix: 'Documents',
+      files: '**/*.stories.mdx',
+    },
+    {
+      directory: '../src/stories',
+      titlePrefix: 'Components',
+      files: '**/*.stories.@(js|jsx|ts|tsx)',
+    },
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
