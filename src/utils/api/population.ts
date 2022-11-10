7ffcc7d95a0dd289e09ab901a59a2d461c9feb65
rmaejima <ryo.maejii@gmail.com>
2022-11-10 13:35:26 +0900

    feat: add population hook

---
+++ src/utils/api/population.ts
@@ -0,0 +1,55 @@
+import { useCallback } from 'react';
+
+import useSWR from 'swr';
+
+import { Population } from 'types/resas/population';
+
+import { requestResasGet } from './axios';
+
+/**
+ *  人口構成取得APIのレスポンスのI/F
+ *  @see https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
+ */
+type PopulationResponse = {
+  message: string | null;
+  result: PopulationResponseResult;
+};
+
+/** 人口構成取得APIのレスポンスのresult部分のI/F */
+type PopulationResponseResult = {
+  boundaryYear: number;
+  data: {
+    label: string;
+    data: Population[];
+  };
+};
+
+/** 都道府県の年単位の人口構成を取得する関数 */
+export const getPopulation = async (
+  prefCode: number,
+): Promise<Population[]> => {
+  const { data } = await requestResasGet<PopulationResponse>(
+    `/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`,
+  );
+  const population = data.result.data.data;
+
+  return population;
+};
+
+/** 都道府県の年単位の人口構成を取得するフック */
+export const usePopulation = (prefCode: number) => {
+  const { data, error, mutate } = useSWR(
+    [prefCode, '/api/v1/population/composition/perYear'],
+    getPopulation,
+  );
+
+  const refetchPopulation = useCallback(() => {
+    mutate();
+  }, [mutate]);
+
+  return {
+    data,
+    error,
+    refetchPopulation,
+  };
+};
