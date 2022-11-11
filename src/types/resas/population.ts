f8a075a988198e3050d80e73fd4e472a20a5eee4
rmaejima <ryo.maejii@gmail.com>
2022-11-11 12:23:58 +0900

    feat: add PrefecturePopulations I/F

--- src/types/resas/population.ts
+++ src/types/resas/population.ts
@@ -1,5 +1,12 @@
-/** 県の人口構成I/F */
+import { Prefecture } from './prefecture';
+
+/** 人口構成I/F */
 export type Population = {
   year: number;
   value: number;
+};
+
+/** 県と人口構成I/F */
+export type PrefecturePopulations = Prefecture & {
+  populations: Population[];
 };
