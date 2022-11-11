bf5924fa51737f77961c3603817ca12760d9fe2c
rmaejima <ryo.maejii@gmail.com>
2022-11-11 10:33:45 +0900

    feat: make show prefecture section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
-import { Header } from 'components/common/Header';
+import { ErrorBoundary } from 'react-error-boundary';
+import styled from 'styled-components';
+
+import { BaseLayout } from 'components/common/BaseLayout';
+import { PrefectureSelectorSection } from 'components/pages/top/PrefectureSelectorSection';
+
+import { Prefecture } from 'types/resas/prefecture';
 
 const App: React.FC = () => {
+  const [selectedPrefecture, setSelectedPrefecture] = useState<Prefecture[]>(
+    [],
+  );
+
+  // TODO: 確認用のため削除
+  useEffect(() => {
+    console.log(selectedPrefecture);
+  }, [selectedPrefecture]);
+
   return (
-    <div>
-      <Header />
-    </div>
+    <BaseLayout>
+      <PageContainer>
+        <SectionWrapper>
+          <ErrorBoundary fallback={<p>error</p>}>
+            <Suspense fallback={<p>now loading</p>}>
+              <PrefectureSelectorSection
+                onChangeSelection={setSelectedPrefecture}
+              />
+            </Suspense>
+          </ErrorBoundary>
+        </SectionWrapper>
+      </PageContainer>
+    </BaseLayout>
   );
 };
 
 export default App;
+
+const PageContainer = styled.div`
+  max-width: ${(p) => p.theme.breaks.lg};
+  margin: 0 auto;
+  padding: 1rem;
+`;
+
+const SectionWrapper = styled.div`
+  margin-bottom: 2rem;
+`;
