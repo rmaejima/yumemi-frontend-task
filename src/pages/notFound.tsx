94626688383e6e5f14fad4608798e33087771415
rmaejima <ryo.maejii@gmail.com>
2022-11-11 15:17:26 +0900

    feat: add not found page

---
+++ src/pages/notFound.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import { BaseLayout } from 'components/common/BaseLayout';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <BaseLayout>
+      <PageContainer>
+        <PageTitle>404 Not Found</PageTitle>
+        <Description>お探しのページが見つかりませんでした。</Description>
+      </PageContainer>
+    </BaseLayout>
+  );
+};
+
+const PageContainer = styled.div`
+  max-width: ${(p) => p.theme.breaks.lg};
+  margin: 0 auto;
+  padding: 1rem;
+`;
+
+const PageTitle = styled.h2`
+  margin-bottom: 0.5rem;
+  font-size: 1.25rem;
+`;
+
+const Description = styled.p`
+  font-size: 1rem;
+`;
