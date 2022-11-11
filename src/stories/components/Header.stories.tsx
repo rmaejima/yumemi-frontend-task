9fbac4285c41bc401d4d9f94949496d4cd7180b9
rmaejima <ryo.maejii@gmail.com>
2022-11-11 17:12:01 +0900

    feat: add Header storybook

--- src/stories/components/Header.stories.tsx
+++ src/stories/components/Header.stories.tsx
@@ -5,8 +5,7 @@ import { ComponentMeta } from '@storybook/react';
 import { Header } from 'components/common/Header';
 
 export default {
-  title: 'Header',
   component: Header,
 } as ComponentMeta<typeof Header>;
 
-export const CommonHeader = () => <Header />;
+export const Index = () => <Header />;
