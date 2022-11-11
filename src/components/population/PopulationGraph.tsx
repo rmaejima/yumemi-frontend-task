bcc53db6ce137d2a7ee1c43e3c658da54ba48b8e
rmaejima <ryo.maejii@gmail.com>
2022-11-11 12:30:54 +0900

    feat: add PopulationGraph component

---
+++ src/components/population/PopulationGraph.tsx
@@ -0,0 +1,95 @@
+import React, { useMemo } from 'react';
+
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import NoDataToDisplay from 'highcharts/modules/no-data-to-display';
+
+import { colors } from 'utils/theme';
+
+import { PrefecturePopulations } from 'types/resas/population';
+
+const MIN_YEAR = 1980;
+const MAX_YEAR = new Date().getFullYear();
+
+/** グラフオプション */
+const options: Highcharts.Options = {
+  chart: {
+    type: 'spline',
+    backgroundColor: 'transparent',
+    polar: true,
+  },
+  title: {
+    text: '',
+  },
+  lang: {
+    noData: 'データがありません',
+  },
+  noData: {
+    style: {
+      fontWeight: 'bold',
+      fontSize: '1rem',
+      color: colors.text.base,
+    },
+  },
+  xAxis: {
+    offset: 0,
+    title: {
+      text: '年度',
+      align: 'high',
+      textAlign: 'left',
+      rotation: 0,
+      offset: 0,
+      margin: 0,
+      y: 30,
+      x: -27,
+    },
+  },
+  yAxis: {
+    visible: true,
+    tickPosition: 'inside',
+    offset: 0,
+    title: {
+      text: '人口数',
+      align: 'high',
+      textAlign: 'left',
+      rotation: 0,
+      offset: 0,
+      margin: 0,
+      y: -20,
+      x: -47,
+    },
+  },
+};
+
+NoDataToDisplay(Highcharts);
+
+type Props = {
+  prefecturePopulations: PrefecturePopulations[];
+};
+
+/** 人口構成グラフコンポーネント */
+export const PopulationGraph: React.FC<Props> = ({ prefecturePopulations }) => {
+  const series = useMemo(() => {
+    if (prefecturePopulations.length === 0) {
+      return [{ data: [], showInLegend: false }];
+    }
+
+    return prefecturePopulations.map((pref) => ({
+      id: pref.prefCode,
+      index: pref.prefCode,
+      name: pref.prefName,
+      data: pref.populations
+        .filter((p) => p.year >= MIN_YEAR && p.year <= MAX_YEAR)
+        .map((p) => [p.year, p.value]),
+      showInLegend: true,
+    }));
+  }, [prefecturePopulations]);
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      constructorType={'chart'}
+      options={{ ...options, series }}
+    />
+  );
+};
