48be0300e7eca07763fd041d6325ed0d90a3ac0c
rmaejima <ryo.maejii@gmail.com>
2022-11-12 14:46:28 +0900

    feat: validate prefecture api response with zod

--- src/utils/api/prefecture.ts
+++ src/utils/api/prefecture.ts
@@ -1,33 +1,44 @@
 import { useCallback } from 'react';
 
 import useSWR from 'swr';
+import { z } from 'zod';
 
-import { Prefecture, ResasErrorResponse } from 'schemas/resas';
+import {
+  Prefecture,
+  prefectureSchema,
+  resasErrorResponseSchema,
+} from 'schemas/resas';
 
 import { requestResasGet } from './axios';
 import { handleResasApiError } from './errorHandler';
 
 /**
- *  都道府県一覧APIレスポンスI/F
+ *  都道府県一覧APIレスポンススキーマ
  *  @see https://opendata.resas-portal.go.jp/docs/api/v1/prefectures.html
  */
-type PrefecturesResponse = {
-  message: string | null;
-  result: Prefecture[];
-};
+const prefecturesResponseSchema = z.object({
+  message: z.string().nullable(),
+  result: z.array(prefectureSchema),
+});
+/** 都道府県一覧APIレスポンスI/F  */
+export type PrefecturesResponse = z.infer<typeof prefecturesResponseSchema>;
 
 /** 都道府県一覧取得する関数 */
 export const getPrefectures = async (): Promise<Prefecture[] | undefined> => {
-  const { data } = await requestResasGet<
-    PrefecturesResponse | ResasErrorResponse
-  >('/api/v1/prefectures');
+  const { data } = await requestResasGet('/api/v1/prefectures');
 
-  if ('statusCode' in data) {
-    handleResasApiError(data);
+  const errorResponse = resasErrorResponseSchema.safeParse(data);
+  if (errorResponse.success) {
+    handleResasApiError(errorResponse.data);
     return;
   }
 
-  return data.result;
+  try {
+    const prefecturesResponse = prefecturesResponseSchema.parse(data);
+    return prefecturesResponse.result;
+  } catch {
+    throw new Error('都道府県一覧取得に失敗しました');
+  }
 };
 
 /** 都道府県一覧取得フック */
