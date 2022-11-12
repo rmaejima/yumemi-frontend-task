d5758cbbc858fe7b9c299bfa10d69474fa1d6292
rmaejima <ryo.maejii@gmail.com>
2022-11-12 14:46:45 +0900

    feat: validate population api response with zod

--- src/utils/api/population.ts
+++ src/utils/api/population.ts
@@ -1,52 +1,65 @@
 import { useCallback } from 'react';
 
 import useSWR from 'swr';
+import { z } from 'zod';
 
 import {
   Population,
   Prefecture,
   PrefecturePopulations,
-  ResasErrorResponse,
+  populationSchema,
+  resasErrorResponseSchema,
 } from 'schemas/resas';
 
 import { requestResasGet } from './axios';
 import { handleResasApiError } from './errorHandler';
 
+/** 人口構成取得APIのレスポンスのresult部分のスキーマ */
+const populationResponseResultSchema = z.object({
+  boundaryYear: z.number(),
+  data: z.array(
+    z.object({
+      label: z.string(),
+      data: z.array(populationSchema),
+    }),
+  ),
+});
+
 /**
- *  人口構成取得APIのレスポンスのI/F
+ *  人口構成取得APIのレスポンスのスキーマ
  *  @see https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
  */
-type PopulationResponse = {
-  message: string | null;
-  result: PopulationResponseResult;
-};
-
-/** 人口構成取得APIのレスポンスのresult部分のI/F */
-type PopulationResponseResult = {
-  boundaryYear: number;
-  data: {
-    label: string;
-    data: Population[];
-  }[];
-};
+const populationResponseSchema = z.object({
+  message: z.string().nullable(),
+  result: populationResponseResultSchema,
+});
+/** 人口構成取得APIのレスポンスのI/F  */
+export type PopulationResponse = z.infer<typeof populationResponseSchema>;
 
 /** 都道府県の年単位の人口構成を取得する関数 */
 export const getPopulation = async (
   prefCode: number,
 ): Promise<Population[] | undefined> => {
-  const { data } = await requestResasGet<
-    PopulationResponse | ResasErrorResponse
-  >(`/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`);
+  const { data } = await requestResasGet(
+    `/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`,
+  );
 
-  if ('statusCode' in data) {
-    handleResasApiError(data);
+  const errorResponse = resasErrorResponseSchema.safeParse(data);
+  if (errorResponse.success) {
+    handleResasApiError(errorResponse.data);
     return;
   }
 
-  const population =
-    data.result.data.find((data) => data.label === '総人口')?.data ?? [];
+  try {
+    const populationResponse = populationResponseSchema.parse(data);
+    const population =
+      populationResponse.result.data.find((data) => data.label === '総人口')
+        ?.data ?? [];
 
-  return population;
+    return population;
+  } catch {
+    throw new Error('人口構成取得に失敗しました');
+  }
 };
 
 /** 都道府県の年単位の人口構成を取得するフック */
